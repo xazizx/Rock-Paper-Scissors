33b86d23638f71b1231d58e96c75f758b2a4ccb0
Aziz Baubaid <3zuccino@gmail.com>
2023-06-26 17:43:49 +0200

    Initial Commit

---
+++ Rock Paper Scissors/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissors
+//
+//  Created by Aziz Baubaid on 26.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Aziz Baubaid on 26.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
